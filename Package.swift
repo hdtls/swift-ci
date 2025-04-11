6c1514b173202132b326308d64dc4f4a41a627c4
Junfeng Zhang <jeff@tenbits.io>
2025-04-12 00:28:30 +0800

    Update swift-tools-version to 5.9

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "swift-ci",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-ci",
-            targets: ["swift-ci"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift-ci"),
-        .testTarget(
-            name: "swift-ciTests",
-            dependencies: ["swift-ci"]
-        ),
-    ]
+  name: "swift-ci",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "swift-ci",
+      targets: ["swift-ci"])
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "swift-ci"),
+    .testTarget(
+      name: "swift-ciTests",
+      dependencies: ["swift-ci"]
+    ),
+  ]
 )
--- Tests/swift-ciTests/swift_ciTests.swift
+++ Tests/swift-ciTests/swift_ciTests.swift
@@ -1,6 +1,33 @@
-import Testing
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Netbot open source project
+//
+// Copyright (c) 2024 Junfeng Zhang and the Netbot project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Netbot project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 @testable import swift_ci
 
-@Test func example() async throws {
+#if swift(>=6.0)
+  import Testing
+#else
+  import XCTest
+#endif
+
+#if swift(>=6.0)
+  @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
+  }
+#else
+  final class Swift_ciTests: XCTestCase {
+    func testExample() throws {
+
+    }
+  }
+#endif
