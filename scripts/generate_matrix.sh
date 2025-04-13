694b8d2ed54f02bb109d78808e3bfb8e442831b8
Junfeng Zhang <jeff@tenbits.io>
2025-04-13 13:06:14 +0800

    Add support for test on host windows

--- scripts/generate_matrix.sh
+++ scripts/generate_matrix.sh
@@ -28,6 +28,7 @@ matrix_append_definition() {
     '.definitions[.definitions| length] |= . + { "platform": $platform, "runner": $runner, "swift_version": $swift_version, "os_version": $os_version, "pre_build_command": $pre_build_command, "build_command": $command, "build_command_options": $command_options }')
 }
 
+
 # Linux CI Matrix
 if [ "$MATRIX_LINUX_5_9_ENABLED" == "true" ]; then
   matrix_append_definition "Linux" "ubuntu-latest" "5.9" "jammy" "$MATRIX_LINUX_PRE_BUILD_COMMAND" "$MATRIX_LINUX_BUILD_COMMAND" "${MATRIX_LINUX_5_9_BUILD_COMMAND_OPTIONS:-$MATRIX_LINUX_BUILD_COMMAND_OPTIONS}"
@@ -54,22 +55,27 @@ if [ "$MATRIX_LINUX_NIGHTLY_MAIN_ENABLED" == "true" ]; then
 fi
 
 
-# # Windows CI Matrix
+# Windows CI Matrix
 if [ "$MATRIX_WINDOWS_6_0_ENABLED" == "true" ]; then
-  matrix_append_definition "Windows" "windows-2022" "6.0" "windowsservercore-ltsc2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_0_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows" "windows-2022" "6.0" "windows-2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_0_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows windowsservercore" "windows-2022" "6.0" "windowsservercore-ltsc2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_0_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_WINDOWS_6_1_ENABLED" == "true" ]; then
-  matrix_append_definition "Windows" "windows-2022" "6.1" "windowsservercore-ltsc2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_1_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows" "windows-2022" "6.1" "windows-2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_1_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows windowsservercore" "windows-2022" "6.1" "windowsservercore-ltsc2022" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_6_1_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_WINDOWS_NIGHTLY_RELEASE_ENABLED" == "true" ]; then
-  matrix_append_definition "Windows" "windows-2019" "nightly-6.1" "windowsservercore-1809" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_RELEASE_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows" "windows-2019" "nightly-6.1" "windows-2019" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_RELEASE_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows windowsservercore" "windows-2019" "nightly-6.1" "windowsservercore-1809" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_RELEASE_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_WINDOWS_NIGHTLY_MAIN_ENABLED" == "true" ]; then
-  matrix_append_definition "Windows" "windows-2019" "nightly-main" "windowsservercore-1809" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_MAIN_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows" "windows-2019" "nightly-main" "windows-2019" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_MAIN_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "Windows windowsservercore" "windows-2019" "nightly-main" "windowsservercore-1809" "$MATRIX_WINDOWS_PRE_BUILD_COMMAND" "$MATRIX_WINDOWS_BUILD_COMMAND" "${MATRIX_WINDOWS_NIGHTLY_MAIN_BUILD_COMMAND_OPTIONS:-$MATRIX_WINDOWS_BUILD_COMMAND_OPTIONS}"
 fi
 
-echo "$matrix" | jq -c
-# echo "$matrix" | jq .
+
+# echo "$matrix" | jq -c
+echo "$matrix" | jq .
