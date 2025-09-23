a09e8d6d95b27a56653f36d5ec3acfbe25b3adb2
Junfeng Zhange <jeff@tenbits.io>
2025-09-23 21:30:09 +0800

    Customizing macOS runners

--- .github/workflows/scripts/generate_matrix.sh
+++ .github/workflows/scripts/generate_matrix.sh
@@ -18,7 +18,6 @@ matrix='{"definitions": []}'
 
 matrix_append_definition() {
   local platform="$1"
-  local runner="$2"
   local swift_version="$3"
   local os_version="$4"
   local tools_version="$5"
@@ -26,46 +25,43 @@ matrix_append_definition() {
   local command="$7"
   local command_options="$8"
 
-    # Detect multiple runners: split by comma or space
-  if [[ "$runner" == *","* ]] || [[ "$runner" == *" "* ]]; then
-    # Convert to JSON array
-    runner_json=$(jq -nc --arg runners "$runner" '($runners | split("[ ,]+"; "g"))')
-  else
-    # Single string
-    runner_json=$(jq -nc --arg r "$runner" '$r')
-  fi
+  IFS=',' read -ra items <<< "$2"
+  for item in "${items[@]}"; do
+    local runner
+    runner="$(echo "$item" | xargs)"
 
-  matrix=$(echo "$matrix" | jq -c \
-    --arg platform "$platform" \
-    --arg swift_version "$swift_version" \
-    --arg os_version "$os_version" \
-    --arg tools_version "$tools_version" \
-    --arg pre_build_command "$pre_build_command" \
-    --arg command "$command"  \
-    --arg command_options "$command_options" \
-    --argjson runner "$runner_json" \
-    '.definitions[.definitions| length] |= . + { "platform": $platform, "runner": $runner, "swift_version": $swift_version, "os_version": $os_version, "tools_version": $tools_version, "pre_build_command": $pre_build_command, "build_command": $command, "build_command_options": $command_options }')
+    matrix=$(echo "$matrix" | jq -c \
+      --arg platform "$platform" \
+      --arg swift_version "$swift_version" \
+      --arg os_version "$os_version" \
+      --arg tools_version "$tools_version" \
+      --arg pre_build_command "$pre_build_command" \
+      --arg command "$command"  \
+      --arg command_options "$command_options" \
+      --arg runner "$runner" \
+      '.definitions[.definitions| length] |= . + { "platform": $platform, "runner": $runner, "swift_version": $swift_version, "os_version": $os_version, "tools_version": $tools_version, "pre_build_command": $pre_build_command, "build_command": $command, "build_command_options": $command_options }')
+  done
 }
 
 # Matrix (macOS)
 if [ "$MATRIX_MACOS_5_9_ENABLED" == "true" ]; then
-  matrix_append_definition "macOS" "macos-14" "5.9" "Sonoma" "Xcode_15.2" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_5_9_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "macOS" "$MATRIX_MACOS_5_9_RUNS_ON" "5.9" "Sonoma" "Xcode_15.2" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_5_9_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_MACOS_5_10_ENABLED" == "true" ]; then
-  matrix_append_definition "macOS" "macos-14" "5.10" "Sonoma" "Xcode_15.4" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_5_10_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "macOS" "$MATRIX_MACOS_5_10_RUNS_ON" "5.10" "Sonoma" "Xcode_15.4" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_5_10_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_MACOS_6_0_ENABLED" == "true" ]; then
-  matrix_append_definition "macOS" "macos-15" "6.0" "Sequoia" "Xcode_16.2" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_0_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "macOS" "$MATRIX_MACOS_6_0_RUNS_ON" "6.0" "Sequoia" "Xcode_16.2" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_0_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_MACOS_6_1_ENABLED" == "true" ]; then
-  matrix_append_definition "macOS" "macos-15" "6.1" "Sequoia" "Xcode_16.4" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_1_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "macOS" "$MATRIX_MACOS_6_1_RUNS_ON" "6.1" "Sequoia" "Xcode_16.4" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_1_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
 fi
 
 if [ "$MATRIX_MACOS_6_2_ENABLED" == "true" ]; then
-  matrix_append_definition "macOS" "macos-26" "6.2" "Tahoe" "Xcode_26.0" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_2_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
+  matrix_append_definition "macOS" "$MATRIX_MACOS_6_2_RUNS_ON" "6.2" "Tahoe" "Xcode_26.0" "$MATRIX_MACOS_PRE_BUILD_COMMAND" "$MATRIX_MACOS_BUILD_COMMAND" "${MATRIX_MACOS_6_2_BUILD_COMMAND_OPTIONS:-$MATRIX_MACOS_BUILD_COMMAND_OPTIONS}"
 fi
 
 # Matrix (Linux)
